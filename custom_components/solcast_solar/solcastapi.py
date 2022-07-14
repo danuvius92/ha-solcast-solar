0e6b462b1ec8dfff40f18f6334d333ea88b1fa73
danuvius92 <31109294+danuvius92@users.noreply.github.com>
2022-07-14 16:24:39 +0200

    Update code to use divider option

--- custom_components/solcast_solar/solcastapi.py
+++ custom_components/solcast_solar/solcastapi.py
@@ -150,7 +150,7 @@ class SolcastApi:
         """Return a rooftop sites total kw for today"""
         #g = [d for d in self._sites if d['resource_id'] == rooftopid]   
         try:
-            return ((self._data["siteinfo"][rooftopid]['tally'])/2)
+            return ((self._data["siteinfo"][rooftopid]['tally'])/number)
         except Exception:
             return 0
 
@@ -198,7 +198,7 @@ class SolcastApi:
                 if p["period_end"].hour >= h:
                     tot += p["pv_estimate"]
             
-            return (round(tot,2)/2)
+            return (round(tot,2)/number)
         except Exception:
             return 0
     
@@ -224,7 +224,7 @@ class SolcastApi:
         try:
             da = dt.now().replace(minute=0, second=0, microsecond=0).astimezone()
             g = [d for d in self._data["forecasts"] if d['period_end'] == da]   
-            return int(g[0]['pv_estimate'] * 1000 / 2)
+            return int(g[0]['pv_estimate'] * 1000 / number)
         except Exception:
             return 0
 
@@ -232,7 +232,7 @@ class SolcastApi:
         try:
             da = dt.now().replace(minute=0, second=0, microsecond=0).astimezone() + timedelta(hours=1)
             g = [d for d in self._data["forecasts"] if d['period_end'] == da]   
-            return int(g[0]['pv_estimate'] * 1000 / 2)
+            return int(g[0]['pv_estimate'] * 1000 / number)
         except Exception:
             return 0
 
@@ -241,7 +241,7 @@ class SolcastApi:
         try:
             da = dt.now().replace(minute=0, second=0, microsecond=0).date()
             g = [d for d in self._data["forecasts"] if d['period_end'].date() == da]
-            return ((round(sum(z['pv_estimate'] for z in g if z),2))/2)
+            return ((round(sum(z['pv_estimate'] for z in g if z),2))/number)
         except Exception:
             return 0
 
@@ -251,7 +251,7 @@ class SolcastApi:
             da = dt.now().replace(minute=0, second=0, microsecond=0).date()
             g = [d for d in self._data["forecasts"] if d['period_end'].date() == da]
             m = max(z['pv_estimate'] for z in g if z) 
-            return int((m * 1000)/2)
+            return int((m * 1000)/number)
         except Exception:
             return 0
 
@@ -275,7 +275,7 @@ class SolcastApi:
         try:
             da = dt.now().replace(minute=0, second=0, microsecond=0).date() + timedelta(days=1)
             g = [d for d in self._data["forecasts"] if d['period_end'].date() == da]
-            return (round(sum(z['pv_estimate'] for z in g if z),2)/2)
+            return (round(sum(z['pv_estimate'] for z in g if z),2)/number)
         except Exception:
             return 0
 
@@ -285,7 +285,7 @@ class SolcastApi:
             da = dt.now().replace(minute=0, second=0, microsecond=0).date() + timedelta(days=1)
             g = [d for d in self._data["forecasts"] if d['period_end'].date() == da]
             m = max(z['pv_estimate'] for z in g if z) 
-            return int((m * 1000)/2)
+            return int((m * 1000)/number)
         except Exception:
             return 0
 
@@ -519,9 +519,9 @@ class SolcastApi:
             else:
                 if lastv == 0.0:
                     #add the last one
-                    wh_hours[lastk] = ((lastv * 1000)/2)
+                    wh_hours[lastk] = ((lastv * 1000)/number)
 
-                wh_hours[d] = ((v['pv_estimate'] * 1000)/2)
+                wh_hours[d] = ((v['pv_estimate'] * 1000)/number)
                 
                 lastk = d
                 lastv = v['pv_estimate']
